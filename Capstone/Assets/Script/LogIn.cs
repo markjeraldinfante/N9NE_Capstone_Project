09f7bdcf4bbb7a1f4426954705e44a7765ba4dbb
Mark Jerald <jerald_infante23@yahoo.com>
2022-10-26 18:10:51 +0800

    Added Initial Log In

---
+++ Capstone/Assets/Script/LogIn.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogIn : MonoBehaviour
+{
+    [SerializeField] private InputField usernameInput;
+    [SerializeField] private Text welcomeText;
+    [SerializeField] private Text usernameText;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button enterButton;
+    [HideInInspector] public string userName;
+    
+
+    void Awake()
+    {
+   
+       if (PlayerPrefs.HasKey("username"))
+        {
+            // kapag may value na ung username
+            usernameText.text = PlayerPrefs.GetString("username");
+            usernameText.gameObject.SetActive(true);
+            welcomeText.gameObject.SetActive(true);
+            usernameInput.gameObject.SetActive(false);
+            enterButton.gameObject.SetActive(false);
+            playButton.gameObject.SetActive(true);
+
+        }
+
+        else
+        {
+            // kapag wala pang value ung username
+            usernameText.gameObject.SetActive(false);
+            welcomeText.gameObject.SetActive(false);
+            usernameInput.gameObject.SetActive(true);
+            enterButton.gameObject.SetActive(true);
+            playButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void EnterGame()
+    {
+        userName = usernameInput.text;
+        usernameText.text = userName;
+        PlayerPrefs.SetString("username", userName);
+        usernameText.gameObject.SetActive(true);
+        welcomeText.gameObject.SetActive(true);
+        usernameInput.gameObject.SetActive(false);
+        enterButton.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(true);
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            PlayerPrefs.DeleteKey("username");
+        }
+    }
+}
