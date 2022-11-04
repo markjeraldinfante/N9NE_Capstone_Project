7ce7e0225e63ecaefdecdaf914e2c2b29451c870
Mark Jerald <jerald_infante23@yahoo.com>
2022-11-04 20:44:31 +0800

    Added UI

--- Capstone/Assets/Script/LogIn.cs
+++ Capstone/Assets/Script/LogIn.cs
@@ -1,69 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class LogIn : MonoBehaviour
-{
-    [SerializeField] private TMP_InputField usernameInput;
-    [SerializeField] private Text welcomeText;
-    [SerializeField] private Text usernameText;
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button enterButton;
-    [HideInInspector] public string userName;
-    
 
-    void Awake()
+    public class LogIn : MonoBehaviour
     {
-   
-       if (PlayerPrefs.HasKey("username"))
-        {
-            // kapag may value na ung username
-            usernameText.text = PlayerPrefs.GetString("username");
-            usernameText.gameObject.SetActive(true);
-            welcomeText.gameObject.SetActive(true);
-            usernameInput.gameObject.SetActive(false);
-            enterButton.gameObject.SetActive(false);
-            playButton.gameObject.SetActive(true);
 
+        public TMP_InputField UsernameInput;
+        public TextMeshProUGUI UsernameView;
+        public GameObject loginView;
+        public GameObject playView;
+
+        void Start()
+        {
+            if (PlayerPrefs.HasKey(PlayerPrefKeys.PLAYER_NICKNAME))
+            {
+                UsernameView.text = PlayerPrefs.GetString(PlayerPrefKeys.PLAYER_NICKNAME);
+                showPlay();
+            }
+        }
+        public void OnButtonPressLogin()
+        {
+            if (UsernameInput.text.Length >= 1)
+            {
+                PlayerPrefs.SetString(PlayerPrefKeys.PLAYER_NICKNAME, UsernameInput.text);
+                UsernameView.text =  UsernameInput.text;
+            }
+            showPlay();
         }
 
-        else
+        public void OnButtonPressPlay()
         {
-            // kapag wala pang value ung username
-            usernameText.gameObject.SetActive(false);
-            welcomeText.gameObject.SetActive(false);
-            usernameInput.gameObject.SetActive(true);
-            enterButton.gameObject.SetActive(true);
-            playButton.gameObject.SetActive(false);
+            SceneManager.LoadScene(1);
+        }
+
+        public void OnButtonPressChange()
+        {
+            UsernameInput.text = PlayerPrefs.GetString(PlayerPrefKeys.PLAYER_NICKNAME);
+            showLogin();
+        }
+
+        private void showPlay()
+        {
+            loginView.SetActive(false);
+            playView.SetActive(true);
+        }
+        private void showLogin()
+        {
+            loginView.SetActive(true);
+            playView.SetActive(false);
         }
     }
 
-    public void EnterGame()
-    {
-        userName = usernameInput.text;
-        usernameText.text = userName;
-        PlayerPrefs.SetString("username", userName);
-        usernameText.gameObject.SetActive(true);
-        welcomeText.gameObject.SetActive(true);
-        usernameInput.gameObject.SetActive(false);
-        enterButton.gameObject.SetActive(false);
-        playButton.gameObject.SetActive(true);
-        
-    }
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            PlayerPrefs.DeleteKey("username");
-        }
-    }
-}
