4a9ee9203dd43926ecb9e2a88a7c2fa61004954f
Mark Jerald <jerald_infante23@yahoo.com>
2022-11-11 00:44:57 +0800

    Fixed Camera Rotation

--- Capstone/Assets/Script/CameraPivot.cs
+++ Capstone/Assets/Script/CameraPivot.cs
@@ -7,13 +7,16 @@ public class CameraPivot : MonoBehaviour
      public Transform cam;
      [SerializeField] public float camRotateValue = 5f;
 
-   
+    private void Start()
+    {
+        LeanTween.reset();
+    }
     void LateUpdate()
     {
 
         float camRotation = Input.GetAxis("Horizontal")*camRotateValue;
+        //LeanTween.init(900);
         LeanTween.rotateY(cam.transform.gameObject, camRotation, .125f);
-       
 
     }
    
