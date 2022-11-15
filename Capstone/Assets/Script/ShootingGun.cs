7359ccec75b0b62904b93b64681c259339eecd0a
Mark Jerald <jerald_infante23@yahoo.com>
2022-11-15 22:52:34 +0800

    Added Function_mini game

--- Capstone/Assets/Script/ShootingGun.cs
+++ Capstone/Assets/Script/ShootingGun.cs
@@ -7,15 +7,33 @@ public class ShootingGun : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 750f;
+    public bool allowFire;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        allowFire = true;
+    }
+
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+
+        if (Input.GetButtonDown("Fire1") && allowFire)
         {
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, bulletSpeed, 0));
+
+            StartCoroutine(Fire());
+
         }
+
+    }
+
+    IEnumerator Fire()
+    {
+        allowFire = false;
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, bulletSpeed, 0));
+        yield return new WaitForSeconds(.5f);
+        allowFire = true;
+        Destroy(bullet, 1f); ;
 
     }
 }
