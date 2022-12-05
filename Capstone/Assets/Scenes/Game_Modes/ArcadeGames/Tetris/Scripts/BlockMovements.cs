88fd440da3aa5eb9bcbf384c269a6ddc92eb84c8
naevistar <101793456+naevistar@users.noreply.github.com>
2022-12-05 22:45:39 +0800

    adding_register

--- Capstone/Assets/Scenes/Game_Modes/ArcadeGames/Tetris/Scripts/BlockMovements.cs
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/Tetris/Scripts/BlockMovements.cs
@@ -13,11 +13,22 @@ public class BlockMovements : MonoBehaviour
     {
         gameLogic = FindObjectOfType <GameLogic>();
     }
+    void RegisterBlock()
+    {
+        foreach (Transform subBlock in rig.transform)
+        {
+            gameLogic.grid[Mathf.FloorToInt(subBlock.position.x),Mathf.FloorToInt(subBlock.position.y)] = subBlock;
+        }
+    }
     bool CheckValid()
     {
         foreach (Transform subBlock in rig.transform)
         {
-            if(subBlock.transform.position.x >= GameLogic.width || subBlock.transform.position.x <= -1 || subBlock.transform.position.y <= 0 )
+            if(subBlock.transform.position.x >= GameLogic.width || subBlock.transform.position.x <= 0 || subBlock.transform.position.y <= 0 )
+            {
+                return false;
+            }
+            if(subBlock.position.y < GameLogic.height && gameLogic.grid[Mathf.FloorToInt(subBlock.position.x),Mathf.FloorToInt(subBlock.position.y)] !=null)
             {
                 return false;
             }
@@ -38,6 +49,8 @@ public class BlockMovements : MonoBehaviour
                 {
                     movable = false;
                     gameObject.transform.position += new Vector3(0,1,0);
+                    RegisterBlock();
+                    gameLogic.ClearLines();
                     gameLogic.SpawnBlock();
                 }
             }
@@ -50,6 +63,8 @@ public class BlockMovements : MonoBehaviour
                 {
                     movable = false;
                     gameObject.transform.position += new Vector3(0,1,0);
+                    RegisterBlock();
+                    gameLogic.ClearLines();
                     gameLogic.SpawnBlock();
                 }
             }
--- Capstone/Assets/Scenes/Game_Modes/ArcadeGames/Tetris/Scripts/GameLogic.cs
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/Tetris/Scripts/GameLogic.cs
@@ -8,17 +8,56 @@ public class GameLogic : MonoBehaviour
     public static float quickDropTime = 0.05f;
     public static int width = 15, height = 30;
     public GameObject[] blocks;
+    public Transform[,] grid = new Transform [width,height];
     // Start is called before the first frame update
     void Start()
     {
         SpawnBlock();
     }
-
-    // Update is called once per frame
-    void Update()
+public void ClearLines() {
+	for (int y = height-1; y >= 0; y--) {
+		if (IsLineComplete(y)) {
+			DestroyLine(y);
+			MoveLines(y);
+		}
+	}
+}
+    
+    void MoveLines(int y)
     {
-        
+        for (int i = y; i < height; i++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if(grid[x,y+1]!= null)
+                {
+                    grid[x,y]=grid[x,y+1];
+                    grid[x,y].gameObject.transform.position -= new Vector3(0,1,0);
+                    grid[x,y+1] = null;
+                }
+            }
+        }
     }
+  
+    void DestroyLine(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Destroy(grid[x,y].gameObject);
+        }
+    }
+    bool IsLineComplete(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if(grid[x,y]==null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Update is called once per frame
     public void SpawnBlock()
     {
         float guess = Random.Range(0, 1f);
