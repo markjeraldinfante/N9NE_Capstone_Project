b2f19c7960e51014bfcb25eb2fae49292412ab6b
naevistar <101793456+naevistar@users.noreply.github.com>
2022-12-12 00:34:06 +0800

    AddingRandomLevelAndDistance

---
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/DistanceDisplay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class DistanceDisplay : MonoBehaviour
+{
+    public GameObject disDisplay;
+    public int disRun;
+    public bool addingDistance = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(addingDistance == false)
+        {
+            addingDistance = true;
+            StartCoroutine(AddingDistance());
+        }
+    }
+    IEnumerator AddingDistance()
+    {
+        disRun += 1;
+        disDisplay.GetComponent<TextMeshProUGUI>().text= "" + disRun;
+        yield return new WaitForSeconds (0.1f);
+        addingDistance = false;
+    }
+}
--- Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/LevelBoundary.cs
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/LevelBoundary.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LevelBoundary : MonoBehaviour
 {
-    public static float leftSide = -1.07f;
-    public static float rightSide = 1.07f;
+    public static float leftSide = 0.2f;
+    public static float rightSide = 3.5f;
     public float internalLeft;
     public float internalRight;
 
