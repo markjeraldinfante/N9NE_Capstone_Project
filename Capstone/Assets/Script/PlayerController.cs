c454f2b5ed237610186cf30ce45a100912a19077
Mark Jerald <jerald_infante23@yahoo.com>
2022-12-17 07:52:44 +0800

    Added Assets

--- Capstone/Assets/Script/PlayerController.cs
+++ Capstone/Assets/Script/PlayerController.cs
@@ -60,6 +60,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             //Vector3 jump = new Vector3(0f, 2f, 0f);
+            characterAnimation.SetBool("Jump", true);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
@@ -78,6 +79,7 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         isGrounded = true;
+        characterAnimation.SetBool("Jump", false);
     }
 
     private void OnCollisionExit(Collision collision)
