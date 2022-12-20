12d297ee034c30b67e288601afc4e1f7a1c2d287
naevistar <101793456+naevistar@users.noreply.github.com>
2022-12-21 01:53:35 +0800

    adding_score

--- Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/ObstacleCollision.cs
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/ObstacleCollision.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
+    public GameObject levelControls;
+
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<Collider>().enabled=false;
         thePlayer.GetComponent<EndelessMovement>().enabled=false;
+        levelControls.GetComponent<DistanceDisplay>().enabled=false;
+        levelControls.GetComponent<EndScoreDisplay>().enabled=true;
         
     }
 }
