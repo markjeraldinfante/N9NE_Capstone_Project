76b2a15a2ff722e706978c17652debd8a4473dfb
Mark Jerald <jerald_infante23@yahoo.com>
2022-12-21 12:22:35 +0800

    Adjust lifebar enemy

--- Capstone/Assets/Script/BillBoard.cs
+++ Capstone/Assets/Script/BillBoard.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BillBoard : MonoBehaviour
 {
     public Transform cam;
+    private void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+    }
 
     // Update is called once per frame
     void LateUpdate()
