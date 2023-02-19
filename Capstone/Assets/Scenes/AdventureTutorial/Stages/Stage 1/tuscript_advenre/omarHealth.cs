89e324c8dd7f1f4a20c158bb010557eb59a1557b
naevistar <101793456+naevistar@users.noreply.github.com>
2023-02-20 00:34:45 +0800

    making healthbar omar

--- Capstone/Assets/Scenes/AdventureTutorial/Stages/Stage 1/tuscript_advenre/omarHealth.cs
+++ Capstone/Assets/Scenes/AdventureTutorial/Stages/Stage 1/tuscript_advenre/omarHealth.cs
@@ -6,10 +6,12 @@ public class omarHealth : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class omarHealth : MonoBehaviour
     }
     void TakeDamage(int damage)
     {
+        healthBar.SetHealth(currentHealth);
         currentHealth -= damage;
     }
     private void OnTriggerEnter(Collider other)
