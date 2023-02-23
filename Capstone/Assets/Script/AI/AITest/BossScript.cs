8141017c407a5d50922a72917e78dc53119ffc7a
Mark Jerald <jerald_infante23@yahoo.com>
2023-02-23 17:54:04 +0800

    boss ai

--- Capstone/Assets/Script/AI/AITest/BossScript.cs
+++ Capstone/Assets/Script/AI/AITest/BossScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
+    public float minDistanceToPlayer = 3f; // The minimum distance between the boss and player
     public float movementSpeed = 5f; // The movement speed of the boss enemy
     public float attackRange = 2f; // The range at which the boss enemy can attack the player
     public float attackCooldown = 2f; // The time between each attack
@@ -17,6 +18,7 @@ public class BossScript : MonoBehaviour
     private float lastAttackTime = 0f; // The time at which the boss enemy last attacked the player
     private float currentChaseTime = 0f; // The current amount of time the boss enemy has been chasing the player
     private bool canChase = true; // Whether the boss enemy can start chasing the player
+    private bool isAttacking = false;
 
     private void Awake()
     {
@@ -35,10 +37,44 @@ public class BossScript : MonoBehaviour
         // If the player is within the chase range and the boss enemy has not exceeded the maximum chase time
         if (distanceToPlayer <= chaseRange && currentChaseTime <= maxChaseTime && canChase)
         {
-            // Chase the player
-            animator.SetTrigger("chase");
-            transform.position = Vector2.MoveTowards(transform.position, player.position, movementSpeed * Time.deltaTime);
+            // Calculate the direction towards the player
+            Vector2 directionToPlayer = (player.position - transform.position).normalized;
 
+            // Check if the boss is farther away from the player than the minimum distance
+            if (distanceToPlayer > minDistanceToPlayer)
+            {
+                if (distanceToPlayer > minDistanceToPlayer && distanceToPlayer >= attackRange)
+                {
+                    if (!isAttacking)
+                    {
+                        // Move the boss towards the player
+                        animator.SetTrigger("chase");
+                        transform.position = Vector2.MoveTowards(transform.position, player.position, movementSpeed * Time.deltaTime);
+                        if (player.position.x < transform.position.x)
+                        {
+                            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+                        }
+                        else
+                        {
+                            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                        }
+                    }
+                    else
+                    {
+                        // Boss is attacking, stop its movement and only update its rotation
+                        GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        if (player.position.x < transform.position.x)
+                        {
+                            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+                        }
+                        else
+                        {
+                            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                        }
+                    }
+                }
+
+            }
             // Increase the current chase time
             currentChaseTime += Time.deltaTime;
         }
@@ -56,6 +92,7 @@ public class BossScript : MonoBehaviour
         // Check if the player has moved away from the boss enemy's chase range
         if (distanceToPlayer > chaseRange)
         {
+            isAttacking = false;
             // Reset the current chase time
             currentChaseTime = 0f;
 
@@ -66,18 +103,32 @@ public class BossScript : MonoBehaviour
 
     private void Attack()
     {
+        isAttacking = true;
+        Debug.Log("Attacking player!");
+        animator.SetBool("Attack", true);
+        animator.SetInteger("AttackIndex", Random.Range(0, 4));
         // Check if the player is in the boss enemy's line of sight
         Vector2 directionToPlayer = player.position - transform.position;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, Mathf.Infinity, playerLayer);
         if (hit.collider != null && hit.collider.CompareTag("Player"))
         {
             // Damage the player
+
             EntityHealth playerHealth = hit.collider.GetComponent<EntityHealth>();
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(attackDamage);
             }
         }
+        // Call EndAttack() after a delay
+        Invoke("EndAttack", 1f);
+    }
+
+    private void EndAttack()
+    {
+        isAttacking = false;
+        Debug.Log("Ending attack!");
+        animator.SetBool("Attack", false);
     }
 
     private IEnumerator DelayBeforeChase()
