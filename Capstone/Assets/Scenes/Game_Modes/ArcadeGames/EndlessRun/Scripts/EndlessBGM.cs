a8184a6c657e3ab10efef208d97175f39e114acb
naevistar <101793456+naevistar@users.noreply.github.com>
2023-03-03 01:05:03 +0800

    adding game over and sounds

---
+++ Capstone/Assets/Scenes/Game_Modes/ArcadeGames/EndlessRun/Scripts/EndlessBGM.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessBGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        somnium.SoundManager.instance.PlayMusic("LitexBG");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
