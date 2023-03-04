488878ef8eb63a1b2af2f73c049e34d025bce99e
Mark Jerald <jerald_infante23@yahoo.com>
2023-03-04 08:55:33 +0800

    Minor bugfix

--- Capstone/Assets/Script/meleeAttack_adventure.cs
+++ Capstone/Assets/Script/meleeAttack_adventure.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 
 public class meleeAttack_adventure : MonoBehaviour
 {
-    [SerializeField] private baseSurvivalVariant variant;
+
     [SerializeField] private KeyCode attackKey = KeyCode.J;
     // [SerializeField] private float attackSpeed = 2f;
     [SerializeField] private Animator characterAnimation;
@@ -15,28 +15,18 @@ public class meleeAttack_adventure : MonoBehaviour
     private void Start()
     {
         characterAnimation = GetComponent<Animator>();
-        if (variant.isOnline)
-        {
-            view = GetComponent<PhotonView>();
-        }
+
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(attackKey))
         {
-            if (variant.isOnline)
-            {
-                if (view.IsMine)
-                {
-                    characterAnimation.SetTrigger("melee");
-                }
-            }
-            else
-            {
-                characterAnimation.SetTrigger("melee");
 
-            }
+
+            characterAnimation.SetTrigger("melee");
+
+
         }
     }
 
