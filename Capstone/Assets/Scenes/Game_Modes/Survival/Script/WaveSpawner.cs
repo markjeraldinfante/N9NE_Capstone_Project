dfe6aec2fdef8b3d290894a7f58cd0ab6483e560
Mark Jerald <jerald_infante23@yahoo.com>
2023-03-31 21:33:35 +0800

    adjust wavemanager

--- Capstone/Assets/Scenes/Game_Modes/Survival/Script/WaveSpawner.cs
+++ Capstone/Assets/Scenes/Game_Modes/Survival/Script/WaveSpawner.cs
@@ -24,40 +24,17 @@ public class WaveSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (variant.variantType != baseSurvivalVariant.VariantType.Online)
+        if (currentEnemy == 0)
         {
-            if (currentEnemy == 0)
+            if (countdown <= 0f)
             {
-                if (countdown <= 0f)
-                {
-                    StartCoroutine(Spawnwave());
-                    waveNumbertext.text = "Wave: " + waveNumber.ToString();
-                    countdown = timeBetweenWaves;
-                }
-                countdown -= Time.deltaTime;
-                waveCountdownText.text = string.Format("{0:00.00}", countdown);
+                StartCoroutine(Spawnwave());
+                waveNumbertext.text = "Wave: " + waveNumber.ToString();
+                countdown = timeBetweenWaves;
             }
+            countdown -= Time.deltaTime;
+            waveCountdownText.text = string.Format("{0:00.00}", countdown);
         }
-        else
-        {
-            if (PhotonNetwork.IsMasterClient)
-            {
-                if (currentEnemy == 0)
-                {
-                    if (countdown <= 0f)
-                    {
-                        StartCoroutine(OnlineSpawnwave());
-                        waveNumbertext.text = "Wave: " + waveNumber.ToString();
-                        countdown = timeBetweenWaves;
-                    }
-                    countdown -= Time.deltaTime;
-                    waveCountdownText.text = string.Format("{0:00.00}", countdown);
-                }
-            }
-
-        }
-
-
     }
 
     IEnumerator Spawnwave()
@@ -68,20 +45,6 @@ public class WaveSpawner : MonoBehaviour
         {
 
             SpawnEnemy();
-            yield return new WaitForSeconds(0.5f);
-
-
-        }
-
-    }
-    IEnumerator OnlineSpawnwave()
-    {
-
-        waveNumber++;
-        for (int i = 0; i < waveNumber; i++)
-        {
-
-            OnlineSpawnEnemy();
             yield return new WaitForSeconds(0.5f);
 
 
@@ -99,23 +62,17 @@ public class WaveSpawner : MonoBehaviour
         Random.Range(spawnPoints1.position.y, spawnPoints2.position.y),
         Random.Range(spawnPoints1.position.z, spawnPoints2.position.z)
         );
-        enemyTrans = Instantiate(enemyPrefab, spawnPointsRandom, spawnPoints1.rotation);
-        currentEnemy++;
-
+        if (variant.variantType == baseSurvivalVariant.VariantType.Online)
+        {
+            enemyTrans = PhotonNetwork.Instantiate(enemyPrefab.name, spawnPointsRandom, spawnPoints1.rotation);
+            currentEnemy++;
+        }
+        else
+        {
+            enemyTrans = Instantiate(enemyPrefab, spawnPointsRandom, spawnPoints1.rotation);
+            currentEnemy++;
+        }
     }
-    public void OnlineSpawnEnemy()
-    {
-        enemyTrans = enemyPrefab;
-        enemyPrefab.GetComponent<ENEMY1>().target = targetEnemy;
-        spawnPointsRandom = new Vector3
-        (
-        Random.Range(spawnPoints1.position.x, spawnPoints2.position.x),
-        Random.Range(spawnPoints1.position.y, spawnPoints2.position.y),
-        Random.Range(spawnPoints1.position.z, spawnPoints2.position.z)
-        );
-        enemyTrans = PhotonNetwork.Instantiate(enemyPrefab.name, spawnPointsRandom, spawnPoints1.rotation);
-        currentEnemy++;
 
-    }
 
 }
