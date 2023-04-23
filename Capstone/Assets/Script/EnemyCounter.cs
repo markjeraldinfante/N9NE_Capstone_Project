ba1369d92ad0631f9cee08f604d29933f14ad44b
Mark Jerald <jerald_infante23@yahoo.com>
2023-04-23 21:05:17 +0800

    Apply enemy counter

--- Capstone/Assets/Script/EnemyCounter.cs
+++ Capstone/Assets/Script/EnemyCounter.cs
@@ -1,34 +1,35 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class EnemyCounter : MonoBehaviour
 {
-    private List<GameObject> enemies; // a list of enemy GameObjects to be tracked
-    [SerializeField] private int enemyCount; // a counter to keep track of how many enemies are left
+    public TextMeshProUGUI enemyCounterText;
+    private List<GameObject> enemies;
+    [SerializeField] private int enemyCount;
 
     void Start()
     {
-        // find all GameObjects in the scene with the "Enemy" tag and add them to the list
+
         enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("enemy"));
-        enemyCount = enemies.Count; // initialize the enemy count to the number of enemies in the list
+        enemyCount = enemies.Count;
     }
 
     void Update()
     {
-        // check if any enemies in the list have been destroyed
+
         for (int i = enemies.Count - 1; i >= 0; i--)
         {
             if (enemies[i] == null)
             {
                 enemies.RemoveAt(i);
-                enemyCount--; // decrease the enemy count when an enemy is destroyed
+                enemyCount--;
             }
         }
-
-        // check if all enemies have been destroyed
         if (enemyCount <= 0)
         {
             Debug.Log("All enemies have been destroyed!");
         }
+        enemyCounterText.text = "Enemy: " + enemyCount.ToString();
     }
 }
